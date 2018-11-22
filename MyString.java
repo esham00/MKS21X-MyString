9b944e4faf38beb550487df1fdc707e7e16625b1
Elizabeth Sham <esham00@stuy.edu>
2018-11-22 15:39:08 -0500

    fixed subsequence and testing charAt and added exceptions

--- MyString.java
+++ MyString.java
@@ -7,21 +7,28 @@ public class MyString implements CharSequence {
 	}
     }
     public char charAt(int index) {
-	//exception: IndexOutOfBounds
+	//exception: IndexOutOfBounds for index less than zero or more than length-1
+	if (index < 0 || index > data.length-1) {
+	    throw new IndexOutOfBoundsException("Your index cannot be below 0 or longer than the amount of characters in your MyString");
+	}
 	return data[index];
     }
     public int length() {
 	return data.length;
     }
     public CharSequence subSequence(int start, int end) {
-	//exception: IndexOutOfBounds
+	//exception: IndexOutOfBounds for start less than zero or end is more than length-1 or end is less than zero or start is larger than end
+	if (start < 0 || end < 0 || end > data.length-1 || start > end) {
+	    throw new IndexOutOfBoundsException("Your index cannot be below 0 or longer than the amount of characters in your MyString");
+	}
 	String sub = "";
-	for (int i = 0; i < data.length; i++) {
+	for (int i = start; i < end; i++) {
 	    sub += data[i];
 	}
 	return sub;
     }
     public String toString() {
+	//output format should be {char, char, ..., char}
 	String output = "{";
 	for (int i = 0; i < data.length; i++) {
 	    if (i == data.length-1) {
